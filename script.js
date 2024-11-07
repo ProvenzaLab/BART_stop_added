f8a7502367c145dfbd3980c84830136b016d122c
mpd197 <michael.dew@bcm.edu>
2024-11-07 11:58:56 -0600

    Add beep
    Play a beep when the first task starts to facilitate video alignment.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import { taskQuestionnaire } from './task_questionnaire.js';
 
 // Initialize jsPsych
 const jsPsych = initJsPsych();
-
+const beepSound = new Audio('sound/beep.wav'); //I added this.
 
 function runAllTasks() {
 
@@ -12,6 +12,7 @@ function runAllTasks() {
   taskDescription().then(() => {
     console.log("Description finished");
     console.log("Starting task...");
+    beepSound.play(); //I added this
     runTask(jsPsych).then(() => {
       console.log(" task  completed.");
       console.log("questionnaire...");
